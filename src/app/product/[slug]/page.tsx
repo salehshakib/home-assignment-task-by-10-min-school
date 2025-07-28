3ed4c5cd4e38fcb79e8332bce6f8249dd4792b5c
Saleh Shakib <salehshakib.ss@gmail.com>
2025-07-29 00:00:25 +0600

    added suspense

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import CourseBanner from '@/app/product/[slug]/_components/course-banner';
 import CourseCard from '@/app/product/[slug]/_components/course-card';
 import { fetchProductData } from '@/app/services/product-service';
@@ -16,21 +18,23 @@ export default async function ProductPage({ params, searchParams }: ProductPageP
   const { data } = await fetchProductData(lang, slug);
 
   return (
-    <div
-      style={{
-        backgroundImage: 'url(https://cdn.10minuteschool.com/images/ui_%281%29_1716445506383.jpeg)',
-      }}
-      className="h-[250px] bg-cover bg-center bg-no-repeat"
-    >
-      <div className="container mx-auto grid w-full max-w-7xl grid-cols-9 gap-8 py-16">
-        <div className="col-span-6 min-h-screen">
-          <CourseBanner productData={data} />
-        </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div
+        style={{
+          backgroundImage: 'url(https://cdn.10minuteschool.com/images/ui_%281%29_1716445506383.jpeg)',
+        }}
+        className="h-[250px] bg-cover bg-center bg-no-repeat"
+      >
+        <div className="container mx-auto grid w-full max-w-7xl grid-cols-9 gap-8 py-16">
+          <div className="col-span-6 min-h-screen">
+            <CourseBanner productData={data} />
+          </div>
 
-        <div className="col-span-3">
-          <CourseCard productData={data} />
+          <div className="col-span-3">
+            <CourseCard productData={data} />
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
